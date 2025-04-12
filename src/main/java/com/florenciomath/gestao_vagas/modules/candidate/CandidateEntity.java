894ce35453e2c81a182b004b2a724cb6194dd0c7
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2025-04-12 13:35:57 -0300

    feat: Primeiro Passos - Aula: Formatando mensagem de validação.

--- src/main/java/com/florenciomath/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/florenciomath/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,6 +1,7 @@
 package com.florenciomath.gestao_vagas.modules.candidate;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -13,13 +14,14 @@ public class CandidateEntity {
     private UUID id;
     private String name;
 
-    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não pode ter espaços!")
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "O campo [username] não pode ter espaços!")
     private String username;
 
     @Email(message = "O campo [email] deve conter um e-mail válido!")
     private String email;
 
-    @Length(min = 10, max = 100)
+    @Length(min = 10, max = 100, message = "O campo [senha] deve conter entre (10) e (100) caracteres!")
     private String password;
 
     private String description;
