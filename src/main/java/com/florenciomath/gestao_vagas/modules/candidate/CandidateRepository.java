ac339541f8a325df8226a314f5fa997d9ecf18ec
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2025-04-16 21:36:23 -0300

    feat: Primeiro Passos - Aula: Criando Repositório.

---
+++ src/main/java/com/florenciomath/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,9 @@
+package com.florenciomath.gestao_vagas.modules.candidate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+
+}
--- src/main/java/com/florenciomath/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/florenciomath/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,9 @@
 package com.florenciomath.gestao_vagas.modules.candidate.controllers;
 
 import com.florenciomath.gestao_vagas.modules.candidate.CandidateEntity;
+import com.florenciomath.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @PostMapping("/")
-    public void create(@Valid  @RequestBody CandidateEntity candidateEntity) {
-        System.out.println("Candidate: " + candidateEntity.getUsername());
+    public CandidateEntity create(@Valid  @RequestBody CandidateEntity candidateEntity) {
+        return this.candidateRepository.save(candidateEntity);
     }
 }
