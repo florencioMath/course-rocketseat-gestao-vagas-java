9aa1e4f1bd8ad27aa901c1a749d645f28d4a7559
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2025-04-17 20:19:10 -0300

    feat: Primeiro Passos - Aula: Validando usuário já existente.

--- src/main/java/com/florenciomath/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/florenciomath/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package com.florenciomath.gestao_vagas.modules.candidate.controllers;
 
+import com.florenciomath.gestao_vagas.exceptions.UserFoundException;
 import com.florenciomath.gestao_vagas.modules.candidate.CandidateEntity;
 import com.florenciomath.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
@@ -18,6 +19,11 @@ public class CandidateController {
 
     @PostMapping("/")
     public CandidateEntity create(@Valid  @RequestBody CandidateEntity candidateEntity) {
+        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+
         return this.candidateRepository.save(candidateEntity);
     }
 }
