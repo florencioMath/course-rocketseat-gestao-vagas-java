20d04b5d91bbd85158d50300a949db6a999b844e
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2025-05-03 15:29:52 -0300

    feat: Primeiro Passos - Aula: Company Controller.

---
+++ src/main/java/com/florenciomath/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -0,0 +1,32 @@
+package com.florenciomath.gestao_vagas.modules.company.controllers;
+
+import com.florenciomath.gestao_vagas.modules.company.entities.CompanyEntity;
+import com.florenciomath.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+
+    @Autowired
+    private CreateCompanyUseCase createCompanyUseCase;
+
+    @PostMapping("/")
+    public ResponseEntity<Object> create(@RequestBody CompanyEntity companyEntity) {
+        System.out.println("AAAAA");
+        try {
+            System.out.println("SUCCESS");
+            var result = this.createCompanyUseCase.execute(companyEntity);
+            return  ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            System.out.println("ERROR");
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+}
